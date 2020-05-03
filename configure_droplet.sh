ac6d8f374670832e17807806bd39ec6e131a8b84
Jenkins Automation Server <jenkins@linux-deploy.localdomain>
2020-05-03 15:45:10 +0400

    v20

---
+++ configure_droplet.sh
@@ -0,0 +1,19 @@
+db=$1
+dbuser=$2
+dbpass=$3
+ans_bin=/usr/bin/ansible-playbook
+target_hosts=terraform/instance_ip.txt
+
+if [ -z "$db" ] || [ -z "$dbuser" ] || [ -z "$dbpass" ]
+then
+  echo "Usage: $0 [DB_name DBUser DB_Pass]"
+  exit 1
+fi
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+
+#--extra-vars '{"name":"Thor"}'
+$ans_bin ansible/deploy-xcart.yml \
+  --extra-vars "mysql_db=$db mysql_dbuser=$dbuser mysql_dbpass=$dbpass" \
+  -i $target_hosts -u root
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-Deploy x-cart
+
+create_droplet.sh [token] - Create digital ocean droplet
+
+configure_droplet.sh      - Install LEMP and X-Cart to the droplet created by "create_droplet.sh"
+
