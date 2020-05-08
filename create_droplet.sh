904cf30cde33d8fe3d0fea0ea1460397ffa7097e
xeonz15 <xeonz73@gmail.com>
2020-05-08 17:56:24 +0400

    check target dir before git clone

--- create_droplet.sh
+++ create_droplet.sh
@@ -12,11 +12,11 @@ then
   exit 1
 fi
 
-cd terraform
+cd terraform/create_droplet
 $terraform_bin init
 $terraform_bin plan -var "digitalocean_token=$do_token"
 $terraform_bin apply -var "digitalocean_token=$do_token" -auto-approve
-$terraform_bin output instance_ip_addr > instance_ip.txt
+$terraform_bin output instance_ip_addr > ../instance_ip.txt
 echo Waiting $wait_time sec while instance booting...
 sleep $wait_time
-cd ..
+cd ../..
